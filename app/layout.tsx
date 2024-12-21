64a9150b7d3d9b1181299d4cb00af5def5ee5505
yuuto0816 <167593083+yuuto0816@users.noreply.github.com>
2024-12-21 12:09:09 +0900

    変更

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import type { Metadata } from "next";
+import Header from "./_components/Header";
+import Footer from "./_components/Footer";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("http://localhost:3000"),
+  title: {
+    template: "%s | シンプルなコーポレートサイト",
+    default: "シンプルなコーポレートサイト",
+  },
+  description:
+  "「Next.js＋ヘッドレスCMSではじめる！かんたん・モダンWebサイト制作入門」で作成されるサイトです。",
+  openGraph: {
+    title: "シンプルなコーポレートサイト",
+    description:
+    "「Next.js＋ヘッドレスCMSではじめる！かんたん・モダンWebサイト制作入門」で作成されるサイトです。",
+    images: ["/ogp.png"],
+  },
+  alternates: {
+    canonical: "http://localhost:3000",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+      <GoogleAnalytics gaId="G-XXX" />
+    </html>
+  );
+}
