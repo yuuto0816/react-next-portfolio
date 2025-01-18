21f163723f7003e9d483ffc1ad57c6d38104d05b
yuuto0816 <167593083+yuuto0816@users.noreply.github.com>
2025-01-18 19:28:04 +0900

    フッター変更

--- app/_components/Footer/index.tsx
+++ app/_components/Footer/index.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
                     </li>
                 </ul>
             </nav>
-            <p className={styles.cr}>© SIMPLE. All Rights Reserved 2024</p>
+            <p className={styles.cr}>© MIKI YUTO. All Rights Reserved 2024</p>
         </footer>
     );
 }
--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -9,7 +9,7 @@ export default function Header() {
             <Link href="/" className={styles.logoLink}>
                 <Image
                     src="/logo.svg"
-                    alt="SIMPLE"
+                    alt="icon"
                     className={styles.logo}
                     width={348}
                     height={133}
