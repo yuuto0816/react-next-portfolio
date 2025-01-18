51f969e992032439a1c239833b36a9ffabea51fa
yuuto0816 <167593083+yuuto0816@users.noreply.github.com>
2025-01-18 17:45:30 +0900

    変更

--- app/hobbies/layout.tsx
+++ app/hobbies/layout.tsx
@@ -2,18 +2,18 @@ import Hero from "@/app/_components/Hero";
 import Sheet from "@/app/_components/Sheet";
 
 export const metadata = {
-  title: "趣味",
+    title: "趣味",
 };
 
 type Props = {
-  children: React.ReactNode;
+    children: React.ReactNode;
 };
 
 export default function RootLayout({ children }: Props) {
-  return (
-    <>
-      <Hero title="hobby" sub="趣味" />
-      <Sheet>{children}</Sheet>;
-    </>
-  );
+    return (
+        <>
+            <Hero title="hobby" sub="趣味" />
+            <Sheet>{children}</Sheet>;
+        </>
+    );
 }
