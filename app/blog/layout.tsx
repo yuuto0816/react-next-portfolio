759645b93682d545cf64dcf8eaa0911952b99b51
yuuto0816 <167593083+yuuto0816@users.noreply.github.com>
2025-01-18 16:25:44 +0900

    変更

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -14,7 +14,7 @@ export const revalidate = 60;
 export default function NewsLayout ({ children }: Props) {
   return (
     <>
-    <Hero title="News" sub="ニュース" />
+    <Hero title="Blog" sub="ブログ" />
     <Sheet>{children}</Sheet>
     </>
   );
