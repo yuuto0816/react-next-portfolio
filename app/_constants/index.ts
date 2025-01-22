f59274bd33fcd769f987fe7e90bf399d5c2c92c8
yuuto0816 <167593083+yuuto0816@users.noreply.github.com>
2025-01-22 13:34:17 +0900

    エラー解決

--- app/_constants/index.ts
+++ app/_constants/index.ts
@@ -1,3 +1,3 @@
-export const MEMBERS_LIST_LIMIT = 100;
-export const TOP_NEWS_LIMIT = 2;
-export const NEWS_LIST_LIMIT = 10;
+export const HOBBIES_LIST_LIMIT = 100;
+export const TOP_BLOG_LIMIT = 2;
+export const BLOG_LIST_LIMIT = 10;
